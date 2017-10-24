babe66af76fbc780271102bf5ab809b1b0052789
Natalie Perpepaj <perpepajn25@gmail.com>
2017-10-24 13:35:04 -0400

    natalies

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,16 @@ def home
   allDistances = Task.all.collect do |task|
       "#{task.lat},#{task.lng}"
     end.join("|")
-  url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + "#{currentLocation}" + "&destinations=" + "#{allDistances}" + "&key="
+  url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + "#{currentLocation}" + "&destinations=" + "#{allDistances}" + "&key= "
   response = RestClient.get(url)
   json = JSON.parse(response)
   json['task'] = @tasks
   render json: json
+end
+
+def create
+  @task = Task.create(charity_id: 1, title: params["title"], description: params["description"], address: params["description"], category: params["category"], date: params["date"], lat: params["lat"], lng:  params["lng"], max_people: params["max_people"])
+  render json: @task
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "/home", to: "tasks#home"
+  post "/create", to: "tasks#create"
 end
